13805ef2ed453d8ca328496cd2beeb74c1965943
Estíver <estiverherrerahipolito@gmail.com>
2012-11-09 21:58:59 -0200

    Update src/main/java/test1/App.java

--- src/main/java/test1/App.java
+++ src/main/java/test1/App.java
@@ -7,6 +7,6 @@ package test1;
 public class App {
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
-        System.out.println( "elemento!" );
+        System.out.println( "elemento111!" );
     }
 }
